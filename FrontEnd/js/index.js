2bb9f81a1b01ced4fc2cc94881173903f77a55b9
Lo0p6TheTardigrade <67520884+Lo0p6TheTardigrade@users.noreply.github.com>
2023-05-24 23:10:47 +0200

    Create all elements tags get all images and titles for the home page(CLEANUP CODE)

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -2,43 +2,6 @@ fetch('http://localhost:5678/api/works')
   .then((response) => response.json())
   .then((works) => console.log(works));
 
-// Get section introduction tag from ID attribute
-const sectionIntroduction = document.getElementById('introduction');
-
-// get article tag by tag name
-const sectionArticle = document.querySelector('article');
-
-// Get section portfolio tag from ID attribute
-const sectionPortfolio = document.getElementById('portfolio');
-
-// Get gallery by css class
-const sectionPortfolioDivGallery = document.querySelector('.gallery');
-
-// let i = 0;
-// fetch('http://localhost:5678/api/works')
-//   .then((response) => response.json())
-//   .then((works) => {
-//     let i = 0;
-//     let imageUrl = works[i].imageUrl;
-//     console.log(imageUrl);
-//     works.forEach((i) => {
-//       // Initialize i
-//       i++;
-
-//       // Create new elements for section portfolio
-//       const sectionPortfolioFigure = document.createElement('figure');
-//       const sectionPortfolioFigureImg = document.createElement('img');
-//       const sectionPortfolioFigcaption = document.createElement('figcaption');
-
-//       // Append elements for the gallery section
-//       sectionPortfolioDivGallery.appendChild(sectionPortfolioFigure);
-//       sectionPortfolioDivGallery.appendChild(sectionPortfolioFigureImg);
-//       sectionPortfolioDivGallery.appendChild(sectionPortfolioFigcaption);
-
-//       sectionPortfolioFigureImg.setAttribute('src', imageUrl);
-//     });
-//   });
-
 fetch('http://localhost:5678/api/works')
   .then((response) => response.json())
   .then((works) => {
@@ -63,6 +26,18 @@ fetch('http://localhost:5678/api/works')
       sectionPortfolioFigcaption.innerText = getTitleForFigcaption;
     });
   });
+
+// Get section introduction tag from ID attribute
+const sectionIntroduction = document.getElementById('introduction');
+
+// get article tag by tag name
+const sectionArticle = document.querySelector('article');
+
+// Get section portfolio tag from ID attribute
+const sectionPortfolio = document.getElementById('portfolio');
+
+// Get gallery by css class
+const sectionPortfolioDivGallery = document.querySelector('.gallery');
 
 // Console log
 console.log(sectionIntroduction, sectionArticle, sectionPortfolio, sectionPortfolioDivGallery);
