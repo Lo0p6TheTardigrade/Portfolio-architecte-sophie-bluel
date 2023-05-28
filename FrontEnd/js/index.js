4e05994920fdde81b5900106d888f9fea8702dcc
Lo0p6TheTardigrade <67520884+Lo0p6TheTardigrade@users.noreply.github.com>
2023-05-28 02:16:09 +0200

    Get category for filters 3/3

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -9,11 +9,14 @@ fetch('http://localhost:5678/api/works')
       const getImageUrl = work.imageUrl;
       const getTitleForFigcaption = work.title;
       const getTitleForAlt = work.title;
+      const getCategoryId = work.categoryId;
+      console.log(getCategoryId);
 
       // Create new elements for section portfolio
       const sectionPortfolioFigure = document.createElement('figure');
       const sectionPortfolioFigureImg = document.createElement('img');
       const sectionPortfolioFigcaption = document.createElement('figcaption');
+      const imageUrlId = parseInt(sectionPortfolioFigureImg.setAttribute('data-id', getCategoryId));
 
       // Append elements for the gallery section
       sectionPortfolioDivGallery.appendChild(sectionPortfolioFigure);
@@ -29,10 +32,13 @@ fetch('http://localhost:5678/api/works')
 
     // Create category elements and append to category div
     const categories = [];
-
+    const allWorksSpan = document.createElement('span');
+    allWorksSpan.textContent = 'Tout';
+    objectsCategoryDiv.appendChild(allWorksSpan);
     works.forEach((work) => {
       const category = work.category;
       const categoryName = category.name;
+      const categoryId = category.id;
 
       if (!categories.includes(categoryName)) {
         categories.push(categoryName);
@@ -40,9 +46,57 @@ fetch('http://localhost:5678/api/works')
         const objectsCategorySpan = document.createElement('span');
         objectsCategorySpan.textContent = categoryName;
         objectsCategoryDiv.appendChild(objectsCategorySpan);
+        objectsCategorySpan.setAttribute('id', categoryId);
+        objectsCategorySpan.setAttribute('data-id', categoryId);
 
-        objectsCategorySpan.addEventListener('click', function () {
-          console.log('trier');
+        objectsCategorySpan.addEventListener('click', () => {
+          const categoryId = parseInt(objectsCategorySpan.getAttribute('data-id'));
+          const filteredWorks = works.filter((work) => work.category.id === categoryId);
+
+          // Reset portfolio
+          sectionPortfolioDivGallery.innerHTML = '';
+
+          // Filter works and create
+          filteredWorks.forEach((work) => {
+            // Create portfolio elements
+            const sectionPortfolioFigure = document.createElement('figure');
+            const sectionPortfolioFigureImg = document.createElement('img');
+            const sectionPortfolioFigcaption = document.createElement('figcaption');
+
+            // Set attributes and text content
+            sectionPortfolioFigureImg.setAttribute('src', work.imageUrl);
+            sectionPortfolioFigureImg.setAttribute('alt', work.title);
+            sectionPortfolioFigureImg.setAttribute('data-id', work.id);
+            sectionPortfolioFigcaption.textContent = work.title;
+
+            // Append portfolio elements
+            sectionPortfolioFigure.appendChild(sectionPortfolioFigureImg);
+            sectionPortfolioFigure.appendChild(sectionPortfolioFigcaption);
+            sectionPortfolioDivGallery.appendChild(sectionPortfolioFigure);
+          });
+        });
+
+        allWorksSpan.addEventListener('click', () => {
+          // Reset for all works
+          sectionPortfolioDivGallery.innerHTML = '';
+
+          // For each works create elements
+          works.forEach((work) => {
+            const sectionPortfolioFigure = document.createElement('figure');
+            const sectionPortfolioFigureImg = document.createElement('img');
+            const sectionPortfolioFigcaption = document.createElement('figcaption');
+
+            // Set attributes and text content
+            sectionPortfolioFigureImg.setAttribute('src', work.imageUrl);
+            sectionPortfolioFigureImg.setAttribute('alt', work.title);
+            sectionPortfolioFigureImg.setAttribute('data-id', work.id);
+            sectionPortfolioFigcaption.textContent = work.title;
+
+            /// Append portfolio elements
+            sectionPortfolioFigure.appendChild(sectionPortfolioFigureImg);
+            sectionPortfolioFigure.appendChild(sectionPortfolioFigcaption);
+            sectionPortfolioDivGallery.appendChild(sectionPortfolioFigure);
+          });
         });
       }
     });
