afd969b782d83bf5ef7a8cc53d4373ed2bb92c72
Lo0p6TheTardigrade <67520884+Lo0p6TheTardigrade@users.noreply.github.com>
2023-06-02 00:16:40 +0200

    JS login.js fetch for login 2a/3

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,23 +1,15 @@
-// fetch('http://localhost:5678/api/users/login')
-//   .then((response) => response.json())
-//   .then((users) => {
-//     users.forEach((user) => {
-//       console.log(user);
-//     });
-//   });
-
 // Find the list item with the following text fields
 let getLoginNavLink = document.getElementsByTagName('*');
 console.log(getLoginNavLink);
 for (let index = 0; index < getLoginNavLink.length; index++) {
   if (getLoginNavLink[index].textContent === 'login') {
-    let login = getLoginNavLink[index];
-    console.log(login);
-    login.setAttribute('id', 'login');
+    let loginNavigation = getLoginNavLink[index];
+    console.log(loginNavigation);
+    loginNavigation.setAttribute('id', 'loginNavigation');
   }
 }
 // Select the list item login
-const loginNavLink = document.getElementById('login');
+let loginNavLink = document.getElementById('loginNavigation');
 const mainTag = document.querySelector('main');
 console.log(loginNavLink);
 
@@ -72,8 +64,6 @@ loginNavLink.addEventListener('click', () => {
   inputPasswordLoginSectionTitle.classList.add('input__style');
   inputPasswordLoginSectionTitle.setAttribute('id', 'password');
   divLabelInputPasswords.appendChild(inputPasswordLoginSectionTitle);
-  let inputUserLogin = inputUserLoginSectionTitle.value;
-  let inputPasswordLogin = inputPasswordLoginSectionTitle.value;
 
   // Create the button for the form
   const buttonLoginSectionTitle = document.createElement('span');
@@ -83,6 +73,9 @@ loginNavLink.addEventListener('click', () => {
   formLoginSectionTitle.appendChild(buttonLoginSectionTitle);
   buttonLoginSectionTitle.addEventListener('click', async () => {
     try {
+      let inputUserLogin = inputUserLoginSectionTitle.value;
+      let inputPasswordLogin = inputPasswordLoginSectionTitle.value;
+
       let response = await fetch('http://localhost:5678/api/users/login', {
         method: 'POST',
         headers: {
@@ -93,21 +86,26 @@ loginNavLink.addEventListener('click', () => {
           password: inputPasswordLogin,
         }),
       });
+      console.log(response);
 
-      let users = await response.json();
-      // Utilisez les données de la réponse ici
-      console.log(users.email);
-      // If user logged
       if (response.ok) {
-        alert('Utilisateur connecté !');
-        login.textContent = 'logout';
+        let data = await response.json();
 
-        // Store all connexion informations in localStorage
-        localStorage.setItem('email', inputUserLoginSectionTitle.value);
-        localStorage.setItem('password', inputPasswordLoginSectionTitle.value);
+        let getTokenResponse = JSON.stringify(data.token);
+        sessionStorage.setItem('token', getTokenResponse);
+        console.log(getTokenResponse);
+        alert('Utilisateur connecté !');
+
+        if (loginNavLink) {
+          // Stockez toutes les informations de connexion dans localStorage
+          localStorage.setItem('email', inputUserLogin);
+          localStorage.setItem('password', inputPasswordLogin);
+
+          // window.location.href = '/FrontEnd';
+          loginNavLink.textContent = 'logout';
+        }
       } else {
-        // connexion fail sent error message
-        alert('Échec de la connexion');
+        alert('Identifiant ou mot de passe incorrect.');
       }
     } catch (error) {
       console.error(error);
@@ -119,42 +117,4 @@ loginNavLink.addEventListener('click', () => {
   linkMissingLoginSectionTitle.textContent = 'Mot de passe oublié';
   linkMissingLoginSectionTitle.style.cursor = 'pointer';
   formLoginSectionTitle.appendChild(linkMissingLoginSectionTitle);
-
-  // Missing link clicked
-
-  linkMissingLoginSectionTitle.addEventListener('click', () => {
-    document.getElementById('main__section').innerHTML = '';
-
-    h2LoginSectionTitle.textContent = 'Mot de passe oublié';
-    mainTag.appendChild(loginSectionTitle);
-    loginSectionTitle.appendChild(articleLoginSectionTitle);
-    articleLoginSectionTitle.appendChild(h2LoginSectionTitle);
-    mainTag.appendChild(formLoginSectionTitle);
-    divLabelInputUser.appendChild(labelUserLoginSectionTitle);
-    inputUserLoginSectionTitle.setAttribute('id', 'username');
-    divLabelInputUser.appendChild(inputUserLoginSectionTitle);
-    buttonLoginSectionTitle.textContent = 'Réinitialiser';
-    labelPasswordLoginSectionTitle.style.display = 'none';
-    inputPasswordLoginSectionTitle.style.display = 'none';
-    linkMissingLoginSectionTitle.style.display = 'none';
-  });
-  // async function fetchData() {
-  //   try {
-  //     let response = await fetch('http://localhost:5678/api/users/login', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         email: inputUserLogin,
-  //         password: inputPasswordLogin,
-  //       }),
-  //     });
-
-  //     let users = await response.json();
-  //     // Utilisez les données de la réponse ici
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
 });
